65cce5edba25726b7e5626a798667892b2467ae0
Kacper Smoliński <kacpersmolinski55@gmail.com>
2026-01-28 19:48:48 +0100

    Create main.py

---
+++ zadanie 4/Sito Eratostenesa/main.py
@@ -0,0 +1,13 @@
+def sito(n):
+    numbers = [True for _ in range(n)]
+    prime_numbers = []
+    for i in range(2, len(numbers)):
+        if i <= 1:
+            numbers[i] = False
+        else:
+            if numbers[i]:
+                prime_numbers.append(i)
+                for j in range(i,len(numbers), i):
+                    numbers[j] = False
+    return prime_numbers
+print(sito(195))
